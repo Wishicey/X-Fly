1cde554dd36c522b98863468875dcd28b89d9c95
Bastien Chanvillard <39124340+Wishicey@users.noreply.github.com>
2018-11-28 15:14:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+        <title>X-Fly</title>
+    </head>
+    <body>
+        <header>
+            <img id="logo" src="" alt="Logo XFly">
+            <input type="button"
+        </header>
+    </body>
+</html>
